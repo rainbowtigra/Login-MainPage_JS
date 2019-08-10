fe17f837638f4da22cfb104329c8b4bf9aed46f6
Oleksii_Volotka <oleksii_volotka@epam.com>
2019-08-10 17:23:51 +0300

    hearts

--- app/js/index.js
+++ app/js/index.js
@@ -1,16 +1,25 @@
 var elements = document.getElementsByClassName("column");
 var i;
 
-// List View
 function listView() {
   for (i = 0; i < elements.length; i++) {
     elements[i].style.width = "100%";
   }
 }
 
-// Grid View
 function gridView() {
   for (i = 0; i < elements.length; i++) {
-    elements[i].style.width = "33%";
+    elements[i].style.width = "30%";
   }
 }
+
+function toggleHeart(element) {
+    if(element.classList.contains('fa-heart-o')){
+        element.classList.remove('fa-heart-o');
+        element.classList.add('fa-heart');
+    } else {
+        element.classList.remove('fa-heart');
+        element.classList.add('fa-heart-o');
+    }
+}
+
