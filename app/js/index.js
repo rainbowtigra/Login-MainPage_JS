380f1ccd5dcab680a76410f5a8b0b0fb807602c9
Oleksii_Volotka <oleksii_volotka@epam.com>
2019-08-11 18:29:28 +0300

    stars work!

--- app/js/index.js
+++ app/js/index.js
@@ -26,7 +26,7 @@ function toggleHeart(element) {
 function renderRating(rating) {
     var star = '';
     for (let i = 0; i < rating; i++) {
-        star += '<i class="fa fa-star"/>'
+        star += '<i class="fa fa-star"></i>'
     }
      return star;
 }
